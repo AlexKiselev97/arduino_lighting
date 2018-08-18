fd5131864114fc6ebe4278b1cdf62d64e6db246f
Alex Kiselev <31107966+AlexKiselev97@users.noreply.github.com>
2018-08-18 20:24:57 +0500

    Update code
    4 modes, code optimization

--- lighting_mega.ino
+++ lighting_mega.ino
@@ -1,10 +1,17 @@
- const int LEFT_PIR = 2;
-const int RIGHT_PIR = 3;
-const int FIRST = 5;
-const int LAST = 12;
-const int lamps[4] = {7, 8, 10, 11};
-const int BUTTON_PULSE_ENDS = 18;
-const int BUTTON_PULSE_ALL = 19;
+// PINS
+//const int LEFT_PIR = 21; //temporarily
+//const int RIGHT_PIR = 20; //temporarily
+const int lamps[] = {7, 8, 9, 10, 11, 12};
+const int FIRST_LAMP = 7; // on the first floor
+const int LAST_LAMP = 12; // on the second floor
+const int BUTTON_MODE_CHANGE = 18;
+// CONST
+const int WAIT_PEOPLE = 15000; // time to move up the stairs
+const int MAX_BRIGHTNESS = 255;
+const int MIN_BRIGHTNESS = 0;
+
+unsigned long startButtonDown = 0;
+
 volatile bool noOne = true;
 volatile bool leftPirActive = false;
 volatile bool rightPirActive = false;
@@ -13,15 +20,13 @@ volatile int mode = 1;
 void setup() {
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
-  pinMode(LEFT_PIR, INPUT);
-  pinMode(RIGHT_PIR, INPUT);
-  pinMode(BUTTON_PULSE_ALL, INPUT_PULLUP);
-  pinMode(BUTTON_PULSE_ENDS, INPUT_PULLUP);
-  delay(15000); //sensor calibration
-  attachInterrupt(digitalPinToInterrupt(LEFT_PIR), leftPirInterrupt, RISING);
-  attachInterrupt(digitalPinToInterrupt(RIGHT_PIR), rightPirInterrupt, RISING);
-  attachInterrupt(digitalPinToInterrupt(BUTTON_PULSE_ENDS), buttonPulseEndsInterrupt, RISING);
-  attachInterrupt(digitalPinToInterrupt(BUTTON_PULSE_ALL), buttonPulseAllInterrupt, RISING);
+  //pinMode(LEFT_PIR, INPUT);
+  //pinMode(RIGHT_PIR, INPUT);
+  pinMode(BUTTON_MODE_CHANGE, INPUT);
+  //delay(15000); // calibration time
+  //attachInterrupt(digitalPinToInterrupt(LEFT_PIR), leftPirInterrupt, CHANGE);
+  //attachInterrupt(digitalPinToInterrupt(RIGHT_PIR), rightPirInterrupt, RISING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_MODE_CHANGE), buttonModeChangeInterrupt, CHANGE);
 }
 
 void leftPirInterrupt()
@@ -36,32 +41,50 @@ void rightPirInterrupt()
   rightPirActive = true;
 }
 
-void buttonPulseEndsInterrupt()
+void buttonModeChangeInterrupt()
 {
-  mode = 1;
-  if (noOne)
-    for (int i=0; i < 4; ++i)
-      analogWrite(lamps[i], 0);
-}
-
-void buttonPulseAllInterrupt()
-{
-  mode = 2;
+  if (startButtonDown == 0 || (millis() - startButtonDown) > 2000)
+    startButtonDown = millis();
+  else
+    return;
+  switch(mode)
+  {
+    case 1:
+    {
+      mode = 2;
+      break;
+    }
+    case 2: 
+    {
+      mode = 3;
+      break;
+    }
+    case 3:
+    {
+      mode = 4;
+      break;
+    }
+    case 4:
+    {
+      mode = 1;
+      break;
+    }
+  }
 }
 
 void leftPirLight()
 {
-  analogWrite(FIRST, 255);
-  analogWrite(LAST, 255);
-  for (int i=0; i < 4; ++i)
-    for (int j=1; j < 256; ++j)
+  analogWrite(FIRST_LAMP, MAX_BRIGHTNESS);
+  analogWrite(LAST_LAMP, MAX_BRIGHTNESS);
+  for (int i=1; i < 5; ++i)
+    for (int j=MIN_BRIGHTNESS; j <= MAX_BRIGHTNESS; ++j)
     {
       analogWrite(lamps[i], j);
       delay(5);
     }
-  delay(15000);
-  for (int i=3; i >= 0; --i)
-    for (int j=255; j >= 0; --j)
+  delay(WAIT_PEOPLE);
+  for (int i=4; i > 0; --i)
+    for (int j=MAX_BRIGHTNESS; j >= MIN_BRIGHTNESS; --j)
     {
       analogWrite(lamps[i], j);
       delay(5);
@@ -69,21 +92,22 @@ void leftPirLight()
   leftPirActive = false;
   rightPirActive = false;
   noOne = true;
+  digitalWrite(13, LOW);
 }
 
 void rightPirLight()
 {
-  analogWrite(FIRST, 255);
-  analogWrite(LAST, 255);
-  for (int i=3; i >= 0; --i)
-    for (int j=1; j < 256; ++j)
+  analogWrite(FIRST_LAMP, 255);
+  analogWrite(LAST_LAMP, 255);
+  for (int i=4; i >= 1; --i)
+    for (int j=MIN_BRIGHTNESS; j <= MAX_BRIGHTNESS; ++j)
     {
       analogWrite(lamps[i], j);
       delay(5);
     }
-  delay(15000);
-  for (int i=0; i < 4; ++i)
-    for (int j=255; j >= 0; --j)
+  delay(WAIT_PEOPLE);
+  for (int i=1; i < 5; ++i)
+    for (int j=MAX_BRIGHTNESS; j >= MIN_BRIGHTNESS; --j)
     {
       analogWrite(lamps[i], j);
       delay(5);
@@ -93,55 +117,109 @@ void rightPirLight()
   noOne = true;
 }
 
-void mode1()
+void modeFirstLastPulse()
 {
-  for (int i=1; i < 256 && noOne; ++i)
+  for (int i=MIN_BRIGHTNESS; i <= MAX_BRIGHTNESS && noOne; ++i)
   {
-      analogWrite(FIRST, i);
-      analogWrite(LAST, i);
+      analogWrite(FIRST_LAMP, i);
+      analogWrite(LAST_LAMP, i);
       delay(12);
   }
-  for (int i=255; i >= 1 && noOne; --i)
+  for (int i=MAX_BRIGHTNESS; i >= MIN_BRIGHTNESS && noOne; --i)
   {
-      analogWrite(FIRST, i);
-      analogWrite(LAST, i);
+      analogWrite(FIRST_LAMP, i);
+      analogWrite(LAST_LAMP, i);
       delay(12);
   }
 }
 
-void mode2()
+void modeAllPulse()
 {
-  for (int i=1; i < 256 && noOne; ++i)
+  for (int i=MIN_BRIGHTNESS; i <= MAX_BRIGHTNESS && noOne; ++i)
   {
-      analogWrite(FIRST, i);
-      analogWrite(LAST, i);
-      for (int j=0; j < 4; ++j)
+      for (int j=0; j < 6; ++j)
         analogWrite(lamps[j], i);
       delay(12);
   }
-  for (int i=255; i >= 1 && noOne; --i)
+  for (int i=MAX_BRIGHTNESS; i >= MIN_BRIGHTNESS && noOne; --i)
   {
-      analogWrite(FIRST, i);
-      analogWrite(LAST, i);
-      for (int j=0; j < 4; ++j)
+      for (int j=0; j < 6; ++j)
         analogWrite(lamps[j], i);
       delay(12);
+  }
+}
+
+void modeWave()
+{
+  for (int i=0; i < 8 && noOne; ++i)
+  { // волна вперед
+      if (i < 6)
+        analogWrite(lamps[i], MAX_BRIGHTNESS);
+      if (i - 1 >= 0)
+        analogWrite(lamps[i-1], MAX_BRIGHTNESS/4);
+      if (i - 2 >= 0)
+        analogWrite(lamps[i-2], MIN_BRIGHTNESS);
+      delay(200);
+  }
+  delay(500);
+  for (int i=7; i >= -1 && noOne; --i)
+  { // волна назад
+      if (i < 6 && i >= 0)
+        analogWrite(lamps[i], MAX_BRIGHTNESS);
+      if (i + 1 < 6)
+        analogWrite(lamps[i+1], MAX_BRIGHTNESS/4);
+      if (i + 2 < 6)
+        analogWrite(lamps[i+2], MIN_BRIGHTNESS);
+      delay(200);
+  }
+}
+
+void modeEven()
+{
+  for (int i=MIN_BRIGHTNESS; i <= MAX_BRIGHTNESS; ++i)
+  {
+    for (int j=0; j < 6; ++j)
+    {
+      if (j%2 != 0)
+        analogWrite(lamps[j], i);
+      else
+        analogWrite(lamps[j], MAX_BRIGHTNESS-i);
+    }
+    delay(12);
+  }
+  
+  for (int i=MIN_BRIGHTNESS; i <= MAX_BRIGHTNESS; ++i)
+  {
+    for (int j=0; j < 6; ++j)
+    {
+      if (j%2 == 0)
+        analogWrite(lamps[j], i);
+      else
+        analogWrite(lamps[j], MAX_BRIGHTNESS-i);
+    }
+    delay(12);
   }
 }
 
 void loop() {
+  for (int i=0; i < 6; ++i)
+      analogWrite(lamps[i], MIN_BRIGHTNESS);
   if (noOne)
   {
     switch(mode)
     {
-      case 1: mode1(); break;
-      case 2: mode2(); break;
+      case 1:
+        modeFirstLastPulse(); break;
+      case 2:
+        modeAllPulse(); break;
+      case 3:
+        modeWave(); break;
+      case 4:
+        modeEven(); break;
     }
   }
   else
   {
-    for (int i=0; i < 4; ++i)
-      analogWrite(lamps[i], 0);
     if (leftPirActive)
       leftPirLight();
     else if (rightPirActive)
